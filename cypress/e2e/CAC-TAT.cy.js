c18311c65d1bbd72b111d765a805e6b42f74c3de
Denis Fernando <denisferoli@gmail.com>
2023-03-30 00:31:19 -0300

    Update CAC-TAT.cy.js

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,3 +1,6 @@
+/// <reference types="Cypress" />
+// Intelisense
+
 describe('', () => {
     beforeEach(() => {
         cy.visit('src/index.html');
@@ -8,6 +11,8 @@ describe('', () => {
     it('Verificar titulo da aplicação', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     });
+
+    //it é o test case
 
     // .should()
 
@@ -20,6 +25,13 @@ describe('', () => {
         cy.get('#email').type('denis@mail.com')
         cy.get('#open-text-area').type('123 Testando som 123')
         cy.get('button[type="submit"]').click()
+    });
+
+    // Adicionar um comando após ao outro chama-se encadear
+    // Aula 03
+
+    it('', () => {
+        
     });
 
 });
