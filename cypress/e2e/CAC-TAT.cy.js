cca2ece41cea30c342f3f0c520f8987360aade59
Denis Fernando <denisferoli@gmail.com>
2023-04-14 00:17:46 -0300

    Update CAC-TAT.cy.js

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -170,7 +170,7 @@ describe('', () => {
 
     // Fazendo upload de arquivos
 
-    it.only('seleciona um arquivo da pasta fixtures', () => {
+    it('seleciona um arquivo da pasta fixtures', () => {
         cy.get('input[type="file"]#file-upload')
         .should('not.have.value')
         .selectFile('cypress/fixtures/example.json')
@@ -179,9 +179,36 @@ describe('', () => {
         })
     });
 
+    // simulando drag and drop
 
-    
-    //parei na aula 30 --------------------------------------------------------------------
+    it('seleciona um arquivo simulando um drag-and0drop', () => {
+        cy.get('input[type="file"]#file-upload')
+        .should('not.have.value')
+        .selectFile('cypress/fixtures/example.json', {action: 'drag-drop'})
+        .should(($input) => {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    });
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]')
+        .selectFile('@sampleFile')
+        .should(($input) => {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    });
+
+    // Abrir outras abas no navegador
+
+    it.only('verifica que a politica de privacidade abre em outra aba sem a necessidade de um clique', () => {
+        cy.get('a')
+    });
+
+
+
+
+    //parei na aula 33 --------------------------------------------------------------------
 
 
 
